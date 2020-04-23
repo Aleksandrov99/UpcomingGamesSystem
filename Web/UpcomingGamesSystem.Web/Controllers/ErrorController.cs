f9a480f3b8431885720b557352df7f9458543bbc
Aleksandrov99 <aleksandrov1999@mail.bg>
2020-04-23 17:14:10 +0300

    Added ErrorController.

---
+++ Web/UpcomingGamesSystem.Web/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿namespace UpcomingGamesSystem.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ErrorController : Controller
+    {
+        [Route("{*url}", Order = 999)]
+        public IActionResult Error404()
+        {
+            this.Response.StatusCode = 404;
+
+            return this.View();
+        }
+    }
+}
